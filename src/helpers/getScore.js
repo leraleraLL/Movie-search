46274cee12157cf6914740b34f2b160df796f5b2
Valeria <79008016+leraleraLL@users.noreply.github.com>
2022-04-08 14:28:53 +0300

    Add files via upload

---
+++ src/helpers/getScore.js
@@ -0,0 +1,10 @@
+export const getScore = (ratingNum) => {
+    const exellent = 7;
+    //const good = 5;
+
+    if (ratingNum > exellent) {
+        return ('./images/score1.png')
+    } else {
+        return ('./images/score1.png')
+    }
+};
